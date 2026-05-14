c378256db23c0a4208cf4a9845d35bb97df95538
Farhan <fa6768243@gmail.com>
2026-05-14 14:37:21 +0500

    style: refine hero section layout and update global styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
+import ServicePage from './pages/ServicePage';
 import Whatsapp from './components/common/Whatsapp';
 
 
@@ -33,6 +34,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'about', element: <About /> },
+      { path: 'services/:serviceId', element: <ServicePage /> },
     ],
   },
 ]);
